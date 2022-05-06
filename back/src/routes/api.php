8bd5df88dfd741379641c071134ec801deb10f01
akihisamakimoto <akihisa.makimoto@dive.design>
2022-05-06 12:09:20 +0900

    API_CORS対応

--- back/src/routes/api.php
+++ back/src/routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'Auth\RegisterController@register');
+
+Route::options('{all}', function () {
+    $response = Response::make('');
+    return $response;
+});
+
+// Auth::routes();
