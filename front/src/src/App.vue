6c769d982b5c32fd8fae74536ba367e2e7d13c37
akihisamakimoto <akihisa.makimoto@dive.design>
2022-05-22 17:16:39 +0900

    WEB_バリデーション修正

--- front/src/src/App.vue
+++ front/src/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
   <HeaderComponent />
-  <!-- <nav>
+  <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/create">Create</router-link>
-  </nav> -->
+  </nav>
   <router-view/>
   </div>
 </template>
--- front/src/src/views/CreatePostView.vue
+++ front/src/src/views/CreatePostView.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="create-post">
-    <form @submit="create">
+    <form @submit.prevent="create">
       <div class="field">
-        <label class="label">タイトル</label>
+        <label class="label">タイトル<span class="has-text-danger">*</span><span class="ml-2 is-size-7">({{ remainingLetterNum.title }} / 50)</span></label>
         <div class="control">
           <input class="input" type="text" v-model="form.title">
         </div>
       </div>
       <div class="field">
-        <label class="label">要約<span class="ml-2">({{ remainingLetterNum }} / 140)</span></label>
+        <label class="label">要約<span class="has-text-danger">*</span><span class="ml-2 is-size-7">({{ remainingLetterNum.description }} / 140)</span></label>
         <div class="control">
           <textarea class="textarea" placeholder="Textarea" v-model="form.description"></textarea>
         </div>
       </div>
       <div class="field">
         <div class="control">
-          <button class="button is-success" type="submit">送信</button>
+          <button class="button is-success" :disabled="!validations.title || !validations.description" type="submit">送信</button>
         </div>
       </div>
     </form>
@@ -31,13 +31,37 @@ export default {
       title: '',
       description: '',
     })
-    const remainingLetterNum = computed(() => {
-      return 140 - form.description.length
+    
+    const remainingLetterNum = reactive({
+      title: computed(() => {
+        return 50 - form.title.length
+      }),
+      description: computed(() => {
+        return 140 - form.description.length
+      })
     })
+
+    const validations = reactive({
+      title: computed(() => {
+        return form.title.length > 0 && form.title.length <= 30
+      }),
+      description: computed(() => {
+        return form.description.length > 0 && form.description.length <= 140
+      })
+    })
+
+    const create = () => {
+      if (Object.values(validations).filter( v => v === false).length) {
+        return
+      }
+      console.log('Create')
+    }
 
     return {
       form,
       remainingLetterNum,
+      validations,
+      create
     }
   },
 }
@@ -47,9 +71,6 @@ export default {
   .create-post {
     label {
       text-align: left;
-      span {
-        font-size: 0.8rem;
-      }
     }
   }
 </style>
